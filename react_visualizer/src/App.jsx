a3ba6db8175f506a093d3edad2e5f7877158ed60
Luis Martinez <2002luismm@gmail.com>
2026-03-21 01:47:36 -0500

    Complete mechanics

---
+++ react_visualizer/src/App.jsx
@@ -0,0 +1,11 @@
+import TelemetryVisualizer from './TelemetryVisualizer'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black p-8">
+      <TelemetryVisualizer />
+    </div>
+  )
+}
+
+export default App
